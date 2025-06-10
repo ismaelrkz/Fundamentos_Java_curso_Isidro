b9e204fa7a5428625a58fd90cf259a2410581afb
ismaelrkz <ismaelreck@gmail.com>
2025-06-10 11:44:09 -0300

    Arquivo Leitura.java adicionado

---
+++ Entrada_de_Dados/Leitura.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Leitura {
+    
+    public static void main(String args[]){
+
+        Scanner teclado = new Scanner(System.in);
+
+        int a;
+        float b;
+        double c;
+
+        System.out.println("Por favor digite um número inteiro!");
+        a = teclado.nextInt();
+        System.out.println("Você digitou o seguinte valor: " + a);
+
+        System.out.println("Por favor digite um número float!");
+        b = teclado.nextFloat();
+        System.out.println("Você digitou o seguinte valor: " + b);
+
+        System.out.println("Por favor digite um número double!");
+        c = teclado.nextDouble();
+        System.out.printf("Você digitou o seguinte valor: %.3f\n", c);
+
+        teclado.close();
+
+    }
+
+}
